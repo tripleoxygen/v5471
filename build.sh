d81afe1446f6cf4889d99272f2d060c375a4a96a
Triple Oxygen <mailme@tripleoxygen.net>
2015-10-21 23:00:34 -0200

    console enabled kernel + initial build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+IMAGE_NAME="B14103-GVT-OXY-"
+BASE_KERNEL_VER=98509
+
+OUTPUT_DIR="build/$1"
+
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
+
+cp bin/header $OUTPUT_DIR/header
+cp bin/kernel"$BASE_KERNEL_VER"con $OUTPUT_DIR/kernel
+
+./build_fs.sh "$1"
+
+cd $OUTPUT_DIR/
+
+cat header kernel rootfs > "$IMAGE_NAME$1.bin"
--- build_fs.sh
+++ build_fs.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 TOOLS_PREFIX=../../..
 
-mkdir -p build/$1
-$TOOLS_PREFIX/squashfs-4.2/mksquashfs squashfs-root/ build/$1/rootfs.squashfs -comp lzma -b 65536 -all-root
+$TOOLS_PREFIX/squashfs-4.2/mksquashfs squashfs-root/ build/$1/rootfs -comp lzma -b 65536 -all-root
