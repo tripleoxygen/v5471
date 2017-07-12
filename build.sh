f221a54375c4e158f74b202d5aa760813ad54a5c
Triple Oxygen <mailme@tripleoxygen.net>
2017-07-11 22:12:06 -0300

    Better build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 IMAGE_NAME="B14103-GVT-OXY-"
-BASE_KERNEL_VER=98509
+BASE_KERNEL_VER=103961
 
 OUTPUT_DIR="build/$1"
 
@@ -18,14 +18,17 @@ OUTPUT_DIR="build/$1"
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
-cp bin/header $OUTPUT_DIR/header
+cp bin/header"$BASE_KERNEL_VER" $OUTPUT_DIR/header
 cp bin/kernel"$BASE_KERNEL_VER"con $OUTPUT_DIR/kernel
 
 ./build_fs.sh "$1"
 
 cd $OUTPUT_DIR/
 
-cat header kernel rootfs > "$IMAGE_NAME$1.bin"
-#cp "$IMAGE_NAME$1.bin" test
-printf 'shsq' | dd of=test bs=1 seek=$(( 0x20a000 )) count=4 conv=notrunc
-printf '\x01' | dd of=test bs=1 seek=$(( 0x20a014 )) count=1 conv=notrunc
+cp rootfs rootfs.patched
+
+# Pace uses a different magic and compression flag
+printf 'shsq' | dd of=rootfs.patched bs=1 seek=$(( 0x00 )) count=4 conv=notrunc
+printf '\x01' | dd of=rootfs.patched bs=1 seek=$(( 0x14 )) count=1 conv=notrunc
+
+cat header kernel rootfs.patched > "$IMAGE_NAME$1.bin"
